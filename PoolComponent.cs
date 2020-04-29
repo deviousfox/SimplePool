54c56e35d1ba3bbde9a25ea0de547f9b79ac3a2a
deviousfox <userMesh@yandex.ru>
2020-04-29 21:53:04 +0300

    V 0.1
    A basic pool system with small snippets of code. In the future we will add: a demo scene, documentation and rewriting the code for the better

---
+++ PoolComponent.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+///   A simple pool component, you can use it separately from PoolController
+/// </summary>
+public class PoolComponent : MonoBehaviour
+{
+    public bool InitializeOnStart;
+    public string Key;
+    public Queue<GameObject> PooledObjects;
+    public GameObject ObjectTemplate;
+    public int ObjectsAmount;
+
+    private void Start()
+    {
+        if (InitializeOnStart)
+        {
+            InitializePool();
+        }
+    }
+    /// <summary>
+    ///   Initialize pool component
+    /// </summary>
+    public void InitializePool()
+    {
+        PooledObjects = new Queue<GameObject>();
+        for (int i = 0; i < ObjectsAmount; i++)
+        {
+            GameObject tempObject = Instantiate(ObjectTemplate);
+            tempObject.SetActive(false);
+            PooledObjects.Enqueue(tempObject);
+        }
+    }
+
+    public GameObject InstantiateFromPool(Vector3 position, Quaternion rotation)
+    {
+        GameObject spawnObject = PooledObjects.Dequeue();
+        spawnObject.transform.position = position;
+        spawnObject.transform.rotation = rotation;
+        spawnObject.SetActive(true);
+
+        return spawnObject;
+    }
+    public void ReturnToPool(GameObject returnedObject)
+    {
+        returnedObject.SetActive(false);
+        PooledObjects.Enqueue(returnedObject);
+    }
+
+
+    public PoolComponent() { }
+    public PoolComponent(GameObject objectTemplate, int objectsAmount)
+    {
+        ObjectTemplate = objectTemplate;
+        ObjectsAmount = objectsAmount;
+        InitializePool();
+    }
+}
---
+++ PoolController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PoolController : MonoBehaviour
+{
+    public bool InitializeOnAwake;
+    public bool FindAllPools;
+    Dictionary<string,PoolComponent> poolDictionary;
+
+    public static PoolController Instance;
+
+    private void Awake()
+    {
+        Instance = this; 
+		if (InitializeOnAwake)
+        {
+            Initialize();
+        }
+    }
+
+
+    public void Initialize()
+    {
+        poolDictionary = new Dictionary<string, PoolComponent>();
+        if (FindAllPools)
+        {
+            PoolComponent[] tempComponents = FindObjectsOfType<PoolComponent>();
+            foreach (PoolComponent component in tempComponents)
+            {
+                poolDictionary.Add(component.Key, component);
+            }
+        }
+    }
+
+    public void InitializeAllPools()
+    {
+        List<PoolComponent> tempComponnents = new List<PoolComponent>();
+        tempComponnents = poolDictionary.Values.ToList();
+        tempComponnents.Union(FindObjectsOfType<PoolComponent>());
+        foreach (PoolComponent component in tempComponnents)
+        {
+            component.InitializePool();
+        }
+    }
+
+    public void InitializePools()
+    {
+        List<PoolComponent> tempComponnents = new List<PoolComponent>();
+        tempComponnents = poolDictionary.Values.ToList();
+
+        foreach (PoolComponent component in tempComponnents)
+        {
+            component.InitializePool();
+        }
+    }
+
+    public GameObject InstantiateFromPool(string key, Vector3 position, Quaternion rotation)
+    {
+        return poolDictionary[key].InstantiateFromPool(position, rotation);
+    }
+
+    public void ReturnToPool(string key, GameObject returnedObject)
+    {
+        poolDictionary[key].ReturnToPool(returnedObject);
+    }
+
+    public PoolComponent GetPool(string key)
+    {
+        if (poolDictionary.ContainsKey(key))
+        {
+            return poolDictionary[key];
+        }
+        else
+        {
+            Debug.LogError("Key " + key + " does not exist.");
+            return null;
+        }
+    }
+
+    public PoolComponent CreatePool(string key, GameObject objectTemplate, int objectsAmount)
+    {
+        if (!poolDictionary.ContainsKey(key))
+        {
+            PoolComponent tempComponent = new PoolComponent(objectTemplate, objectsAmount);
+            poolDictionary.Add(key, tempComponent);
+        }
+        return GetPool(key);
+    }
+
+
+}
